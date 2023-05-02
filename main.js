fc502648a265c6220d0341e88272df5efc6a002d
Kenedy Jardim <kenedydacosta@gmail.com>
2023-05-02 20:53:39 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+    
+
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+
+        const tarefa = $('#tarefa').val();
+        const novaTarefa = $(`<li>${tarefa}</li>`);
+        $(novaTarefa).appendTo('ul');
+
+        $('#tarefa').val('');
+    })
+
+    $(document).on("click", "li", function() {
+        $(this).addClass('riscado');
+    });  
+
+})
+
