0f0d575fc23b99a978d313b5de87d48148b9978f
Kenedy Jardim <kenedydacosta@gmail.com>
2023-11-22 07:35:19 -0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicaNumeros(numeroA, numeroB) {
+    return numeroA * numeroB;
+}
+function saudacao(nome) {
+    return "Olá " + nome;
+}
+console.log(multiplicaNumeros(9, 9));
+console.log(saudacao("Cicrano"));
---
+++ src/main.ts
@@ -0,0 +1,11 @@
+function multiplicaNumeros(numeroA: number, numeroB:number){  //Função Multiplicação
+    return numeroA*numeroB;
+}
+
+function saudacao(nome: string){ //Função Retorna Nome
+    return "Olá "+nome;
+}
+
+console.log(multiplicaNumeros(9, 9));
+
+console.log(saudacao("Cicrano"));
